6b590b378fa1790f2387390fc623b40eb63ebad0
Hiatus <77402029+hiatus770@users.noreply.github.com>
2023-08-20 16:09:07 -0400

    Working space partitioning for ball optimization

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+// 
+
+
 #include <iostream>
 #include <cmath>
 #include <raylib.h>
 #include <raymath.h>
 #include <vector>
 #include <array>
-#define WIDTHGAME 30
-#define HEIGHTGAME 30
+#include <rlgl.h>
+#define WIDTHGAME 25
+#define HEIGHTGAME 25
 #define toDegree(x) (x * 180.0 / 3.14159)
 #define toRadian(x) (x * 3.14159 / 180.0)
 #define gameFPS 500
@@ -16,7 +20,7 @@
 #define dragCoef 0.999
 #define elasticCoef 0.8
 #define collisionCoef 0.4
-#define subSteps 4
+#define subSteps 2
 
 using namespace std;
 
@@ -27,35 +31,7 @@ const int maxHeight = screenHeight;
 const int minWidth = 0;
 const int minHeight = 0;
 
-int colorCounter = 0;
-
-// Define some operators for vector2 that are missing such as + - * /
-Vector2 operator+(const Vector2 &v1, const Vector2 &v2)
-{
-    return {v1.x + v2.x, v1.y + v2.y};
-}
-Vector2 operator-(const Vector2 &v1, const Vector2 &v2)
-{
-    return {v1.x - v2.x, v1.y - v2.y};
-}
-Vector2 operator*(const Vector2 &v1, const Vector2 &v2)
-{
-    return {v1.x * v2.x, v1.y * v2.y};
-}
-Vector2 operator/(const Vector2 &v1, const Vector2 &v2)
-{
-    return {v1.x / v2.x, v1.y / v2.y};
-}
-Vector2 operator*(const Vector2 &v1, const double &v2)
-{
-    return {v1.x * v2, v1.y * v2};
-}
-Vector2 operator/(const Vector2 &v1, const double &v2)
-{
-    return {v1.x / v2, v1.y / v2};
-}
-
-class QuadTree;
+#include "vectorOperators.h"
 
 class Ball
 {
@@ -63,9 +39,9 @@ public:
     Vector2 position;
     Vector2 lastPosition;
     Vector2 velocity;
-    Vector2 acceleration = {0, 0.1};
-    float radius;
-    Color col = RED;
+    const Vector2 acceleration = {0, 0.1};
+    const float radius;
+    const Color col = RED;
 
     Ball(float x, float y, float radius)
     {
@@ -94,10 +70,6 @@ public:
         Vector2 displacement = Vector2Subtract(position, lastPosition);
         lastPosition = position;
         position = position + displacement * dragCoef + acceleration * dt * dt;
-
-        // Vector2 displacement = Vector2Subtract(position, lastPosition);
-        // lastPosition = position;
-        // position = position + displacement*dragCoef + acceleration;
     }
 
     void bounds()
@@ -130,7 +102,7 @@ public:
         DrawCircle(position.x, position.y, radius, col);
     }
 
-    void collision();
+    void collision(int debug);
 
     void setVelocity(Vector2 v)
     {
@@ -145,13 +117,54 @@ public:
 
 vector<Ball> globalBalls;
 
+void ballCoordToGrid(Ball *b, int *x, int *y);
 
-void Ball::collision()
+// The 2d array of the grid
+vector<Ball*> grid[WIDTHGAME][HEIGHTGAME];
+
+/**
+ *  @brief Collision function for balls 
+ *  @return void
+ */
+void Ball::collision(int debug = 0)
 {
-    // Use space partitioning to find the balls that are close to this ball
-    for (int i = 0; i < (int)globalBalls.size(); i++)
+    int x;
+    int y;
+    ballCoordToGrid(this, &x, &y);
+    vector<Ball*> closeBalls;
+    
+    // [ ][b][ ]
+    // [b][a][b]
+    // [ ][b][ ]
+    // A is the current ball, B is all the grids that have to be looked at 
+    for (int i = -1; i <= 1; i++)
     {
-        Ball *b = &globalBalls.at(i);
+        if (x + i >= 0 && x + i < WIDTHGAME)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (y + j >= 0 && y + j < HEIGHTGAME)
+                {
+                    for (int k = 0; k < (int)grid[x + i][y + j].size(); k++)
+                    {
+                        closeBalls.push_back(grid[x + i][y + j].at(k));
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Use space partitioning to find the balls that are close to this ball
+    for (int i = 0; i < (int)closeBalls.size(); i++)
+    {
+        Ball *b = closeBalls.at(i);
+        // Draw a line from the center of this ball to the center of the other ball
+        if (debug == 1)
+        {
+            DrawLineEx(position, b->position, 1, RED);
+        }
+
         if (b != this)
         {
             float length = sqrtf((b->position.x - position.x) * (b->position.x - position.x) + (b->position.y - position.y) * (b->position.y - position.y));
@@ -184,6 +197,47 @@ void Ball::collision()
     }
 }
 
+void ballCoordToGrid(Ball *b, int *x, int *y)
+{
+    *x = (int)(b->position.x / (maxWidth / WIDTHGAME));
+    *y = (int)(b->position.y / (maxHeight / HEIGHTGAME));
+}
+
+/**
+ * @brief  
+ * 
+ */
+void updateGrid(){
+    // Clear the grid
+    for (int i = 0; i < WIDTHGAME; i++)
+    {
+        for (int j = 0; j < HEIGHTGAME; j++)
+        {
+            grid[i][j].clear();
+        }
+    }
+    // Push all the necessary balls into the grid
+    for (int i = 0; i < (int)globalBalls.size(); i++)
+    {
+        Ball *b = &globalBalls.at(i);
+        int x;
+        int y;
+        ballCoordToGrid(b, &x, &y);
+        grid[x][y].push_back(b);
+    }
+}
+
+void drawGrid (){
+    for (int i = 0; i <= WIDTHGAME; i++)
+    {
+        for (int j = 0; j <= HEIGHTGAME; j++)
+        {
+            DrawRectangleLines(i * (maxWidth / WIDTHGAME), j * (maxHeight / HEIGHTGAME), maxWidth / WIDTHGAME, maxHeight / HEIGHTGAME, WHITE);
+            DrawText(TextFormat("%d", i + j * WIDTHGAME), i * (maxWidth / WIDTHGAME), j * (maxHeight / HEIGHTGAME), 10, WHITE);
+        }
+    }
+}
+
 // Main function
 int main()
 {
@@ -191,14 +245,39 @@ int main()
     InitWindow(screenWidth, screenHeight, "Balluh");
     SetTargetFPS(gameFPS);
 
+    Camera2D cam = {0};
+    cam.zoom = 1; 
+
     float lastTime = 0;
 
     // test ball
 
     while (WindowShouldClose() == false)
     {
+
+        // Mouse zoom stuff 
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f / cam.zoom);
+            cam.target = Vector2Add(cam.target, delta);
+        }
+
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0)
+        {
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), cam);
+            cam.offset = GetMousePosition(); 
+            cam.target = mouseWorldPos;
+            cam.zoom += wheel * 0.5f;
+            if (cam.zoom < 0.1f)
+                cam.zoom = 0.1f;
+        }
+
+        // Drawing begins here 
         BeginDrawing();
         ClearBackground(BLACK);
+        BeginMode2D(cam);
+        drawGrid(); 
 
         for (int i = 0; i < (int)globalBalls.size(); i++)
         {
@@ -206,20 +285,21 @@ int main()
             b->draw();
         }
 
-        // add balls on mouse press
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        // Add balls on mouse pressed 
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && globalBalls.size() < 2000)
         {
             Vector2 mousePos = GetMousePosition();
-            // with velocity of the mouse delta
-            globalBalls.push_back(Ball(mousePos.x, mousePos.y, GetMouseDelta()/5, 10));
+            globalBalls.push_back(Ball(mousePos.x, mousePos.y, GetMouseDelta()/1, 3));
         }
 
+        // Update at a constant update rate 
         if (GetTime() - lastTime > 0.01)
         {
             lastTime = GetTime();
             float dt = 1.0 / subSteps;
             for (int subStepCount = 0; subStepCount < subSteps; subStepCount++)
             {
+                updateGrid(); 
                 for (int i = 0; i < (int)globalBalls.size(); i++)
                 {
                     Ball *b = &globalBalls.at(i);
@@ -230,7 +310,6 @@ int main()
                     Ball *b = &globalBalls.at(i);
                     b->update(dt);
                 }
-
                 for (int i = 0; i < (int)globalBalls.size(); i++)
                 {
                     Ball *b = &globalBalls.at(i);
@@ -238,6 +317,8 @@ int main()
                 }
             }
         }
+
+        EndMode2D();
 
         // Display the FPS currently
         DrawFPS(10, 10);
